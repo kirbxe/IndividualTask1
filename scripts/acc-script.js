2311ad300bfaf834897f0973c0a9b3ac9e728184
kirbxeee <129985192+kirbxe@users.noreply.github.com>
2025-04-21 23:55:14 +0500

    Delete scripts directory

--- scripts/acc-script.js
+++
@@ -1,9 +0,0 @@
-const savedUser = JSON.parse(localStorage.getItem('userData'))
-
-
-console.log(savedUser)
-
- document.addEventListener('DOMContentLoaded', () => {
-     const accTitle = document.getElementById('acc-title');
-    accTitle.innerHTML = `Здравствуйте, ${savedUser.login}`;
-  });
